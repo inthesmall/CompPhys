770db352e381358c919016f82ac1352fa05f6c12
Ethan Mills <ethan.mills@hotmail.co.uk>
2017-10-31 17:22:43 +0000

    Added tests for solving simultaneous equations by fb substitution

--- ProjectA/test_matrix.py
+++ ProjectA/test_matrix.py
@@ -18,3 +18,21 @@ class EdgeCase(unittest.TestCase):
 
     def test_edge_det(self):
         self.assertEqual(-1, self.n)
+
+
+class Simul(unittest.TestCase):
+    def setUp(self):
+        self.M = np.matrix('1 2; 1 1')
+        self.b = np.array([[3], [5]])
+        self.P, self.n, self.L, self.U = matrix.LU(self.M)
+
+    def test_simul_LU(self):
+        self.assertFalse(((self.L * self.U) - (self.P * self.M)).any())
+
+    def test_simul_P(self):
+        self.assertFalse((self.P - np.matrix('0 1;1 0')).any())
+
+    def test_simul_solve(self):
+        self.assertFalse(
+            (matrix.fb_sub(self.L, self.U, self.P, self.b) -
+                np.array([7., -2.])).any())
