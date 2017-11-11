80043a22a9f67ef441018accb9000def017d80dc
Ethan Mills <ethan.mills@hotmail.co.uk>
2017-11-11 22:41:06 +0000

    Updated unittests to reflect changes in linear interface

--- ProjectA/test_inter.py
+++ ProjectA/test_inter.py
@@ -8,12 +8,12 @@ class LinearTest(unittest.TestCase):
         self.ys = [0, 1, 2, 3, 4, 5, 6, 7]
 
     def test_trivial_case(self):
-        self.assertEqual(linear.linint(self.xs, self.ys, 0.2), 0.2)
+        self.assertEqual(linear.linint(0.2, self.xs, self.ys), 0.2)
 
     def test_out_of_range_error_above(self):
         with self.assertRaises(ValueError):
-            linear.linint(self.xs, self.ys, 15)
+            linear.linint(15, self.xs, self.ys)
 
     def test_out_of_range_error_below(self):
         with self.assertRaises(ValueError):
-            linear.linint(self.xs, self.ys, -1)
+            linear.linint(-1, self.xs, self.ys)
