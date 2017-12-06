289c3fa441ae84fd045d14d3e26644736d181b66
Ethan Mills <ethan.mills@hotmail.co.uk>
2017-12-06 03:41:47 +0000

    Added heatsink

--- ProjectB/B4/new.py
+++ ProjectB/B4/new.py
@@ -13,7 +13,6 @@ from core import *
 k = np.array([i.get_k() for i in objects.no_heatsink])
 values = np.array([(1, 0)[isinstance(i, objects.Outside)]
                    for i in objects.no_heatsink])
-# values = np.array([1 for i in objects.no_heatsink])
 
 up = np.array([(j, 0)[i % across == 0] for i, j in enumerate(values)])
 down = np.array([(j, 0)[(i + 1) % (across) == 0]
@@ -27,11 +26,9 @@ L = ss.dia_matrix(
     (np.stack((down, values)), [-1, -across]), shape=(total, total))
 
 
-
-
-def get_edges(u):
+def get_edges(u, obj=objects.no_heatsink):
     out = []
-    for i, j in enumerate(objects.no_heatsink):
+    for i, j in enumerate(obj):
         if isinstance(j, objects.OutsideEdge):
             if isinstance(j, objects.Corner):
                 out.append(2 * (u[i] - delta * 1.31E-6 *
@@ -44,7 +41,7 @@ def get_edges(u):
             out.append(0)
     return np.array(out)
     return np.array([i.update(u, j) if isinstance(i, objects.OutsideEdge) else 0
-                     for j, i in enumerate(objects.no_heatsink)])
+                     for j, i in enumerate(obj)])
 
 
 def run(target):
@@ -68,3 +65,36 @@ def run_num(num):
         edges = get_edges(u)
         u = -update * u - (D_inv * (edges + k))
     return u
+
+
+def run_object(obj, target):
+    item = obj.item
+    total = obj.total
+    across = obj.across
+
+    k = np.array([i.get_k() for i in item])
+    values = np.array(
+        [0 if isinstance(i, objects.Outside) else 1 for i in item])
+
+    up = np.array([(j, 0)[i % across == 0] for i, j in enumerate(values)])
+    down = np.array([(j, 0)[(i + 1) % (across) == 0]
+                     for i, j in enumerate(values)])
+    down[0] = values[0]
+
+    D_inv = ss.dia_matrix((values * -0.25, [0]), shape=(total, total))
+    U = ss.dia_matrix(
+        (np.stack((up, values)), [1, across]), shape=(total, total))
+    L = ss.dia_matrix(
+        (np.stack((down, values)), [-1, -across]), shape=(total, total))
+
+    u = np.full(total, 4000.)
+    old = 0
+    iters = 0
+    update = D_inv * (U + L)
+
+    while abs(sum(u) - old) > target:
+        old = sum(u)
+        edges = get_edges(u, obj)
+        u = -update * u - (D_inv * (edges + k))
+        iters += 1
+    return u, iters
--- ProjectB/B4/objects.py
+++ ProjectB/B4/objects.py
@@ -86,6 +86,7 @@ class BottomEdge(OutsideEdge):
     def update(self, u, j):
         return u[j] - delta * 1.31E-6 * (u[j] - 293.15)**(4 / 3)
 
+
 class Corner(OutsideEdge):
     def __init__(self, k, q):
         self.k = k
@@ -183,3 +184,117 @@ no_heatsink = np.concatenate((
     [BottomRightCorner(k1, q)],
     np.full(int(3 / delta), Outside()),
 ))
+
+
+def pad_to_width(item, item_width, width):
+    shape = np.reshape(item, (-1, int(item_width / delta) + 1))
+    height = shape.shape[0]
+    side_pad = (width - item_width) / (2 * delta)
+    side = np.full((height, int(side_pad)), Outside())
+    out = np.concatenate((side, shape, side), 1)
+    return out.reshape(-1)
+
+
+def make_heatsink_a(fins):
+    width = (3 * fins) - 2
+    heatsink = np.concatenate((
+        # Top
+        np.tile(
+            np.concatenate((
+                [TopLeftCorner(k3, 0)],
+                np.tile(TopEdge(k3, 0), int(1 / delta) - 1),
+                [TopRightCorner(k3, 0)],
+                np.tile(Outside(), int(2 / delta) - 1))),
+            fins - 1
+        ),
+        [TopLeftCorner(k3, 0)],
+        np.tile(TopEdge(k3, 0), int(1 / delta) - 1),
+        [TopRightCorner(k3, 0)],
+
+        # Middle
+        np.tile(
+            np.concatenate((
+                np.tile(
+                    np.concatenate((
+                        [LeftEdge(k3, 0)],
+                        np.tile(Inside(k3, 0), int(1 / delta) - 1),
+                        [RightEdge(k3, 0)],
+                        np.tile(Outside(), int(2 / delta) - 1))),
+                    fins - 1
+                ),
+                [LeftEdge(k3, 0)],
+                np.tile(Inside(k3, 0), int(1 / delta) - 1),
+                [RightEdge(k3, 0)]
+            )),
+            int(30 / delta) - 1
+        ),
+
+        # Base
+        [LeftEdge(k3, 0)],
+        np.tile(
+            np.concatenate((
+                np.tile(Inside(k3, 0), int(1 / delta) - 1),
+                [InsideCorner(k3, 0)],
+                np.tile(TopEdge(k3, 0), int(2 / delta) - 1),
+                [InsideCorner(k3, 0)]
+            )),
+            fins - 1
+        ),
+        np.tile(Inside(k3, 0), int(1 / delta) - 1),
+        [RightEdge(k3, 0)],
+        # Ceramic
+        np.tile(
+            np.concatenate((
+                [LeftEdge(k3, 0)],
+                np.tile(Inside(k3, 0), int(width / delta) - 1),
+                [RightEdge(k3, 0)]
+            )),
+            int(4 / delta) - 1
+        )
+    ))
+
+    if width > 20:
+        # pad cpu
+        h = heatsink
+        j = np.concatenate((
+            [BottomLeftCorner(k3, 0)],
+            np.tile(BottomEdge(k3, 0), int((width - 20) / (2 * delta)) - 1),
+            [InsideCorner((2 * k3 + k2) / 3, 0)],
+            np.tile(Boundary(k3, k2, 0), across - 2),
+            [InsideCorner((2 * k3 + k2) / 3, 0)],
+            np.tile(BottomEdge(k3, 0), int((width - 20) / (2 * delta)) - 1),
+            [BottomRightCorner(k3, 0)]
+        ))
+        c = pad_to_width(no_heatsink[int(20 / delta) + 1:], 20, width)
+        return np.concatenate((h, j, c))
+    elif width < 20:
+        # pad heatsink
+        h = pad_to_width(heatsink, width, 20)
+        j = np.concatenate((
+            [TopLeftCorner(k2, 0)],
+            np.tile(TopEdge(k2, 0), int((20 - width) / (2 * delta)) - 1),
+            [InsideCorner((k3 + 2 * k2) / 3, 0)],
+            np.tile(Boundary(k3, k2, 0), int(width / delta) - 1),
+            [InsideCorner((k3 + 2 * k2) / 3, 0)],
+            np.tile(TopEdge(k2, 0), int((20 - width) / (2 * delta)) - 1),
+            [TopRightCorner(k2, 0)]
+        ))
+        c = no_heatsink[int(20 / delta) + 1:]
+        return np.concatenate((h, j, c))
+    else:
+        out = np.append(no_heatsink, heatsink)
+
+    return out
+
+
+class HeatsinkA():
+    def __init__(self, fins, delta=delta):
+        self.item = make_heatsink_a(fins)
+        self.total = len(self.item)
+        self.width = max(20, (3 * fins) - 2)
+        self.height = 37
+        self.across = (self.width / delta) + 1
+        self.down = (self.height / delta) + 1
+
+    def __iter__(self):
+        return self.item.__iter__()
